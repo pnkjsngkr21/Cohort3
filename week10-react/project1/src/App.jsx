4d6b40648a7fea90178066c7537ae53e7ebace13
Pankaj Sangekar <pnkjsngkr21@gmail.com>
2025-11-23 11:57:35 +0530

    adding week 10 projects

---
+++ week10-react/project1/src/App.jsx
@@ -0,0 +1,56 @@
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+
+function App() {
+
+
+  return (
+    <div>
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/neet/class-11" element={<Class11Program />} />
+          <Route path="/neet/class-12" element={<Class12Program />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <>
+            <Link to="/">Home</Link> |
+        <Link to="/neet/class-11">Class 11</Link> |
+        <Link to="/neet/class-12">Class 12</Link>
+        </>
+  )
+}
+
+
+function LandingPage() {
+  return (
+    <h1>Landing Page</h1>
+  )
+}
+
+function Class11Program() {
+
+  return (
+    <h1>Class 11 Program</h1>
+  )
+}
+
+function Class12Program() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <h1>Class 12 Program</h1>
+      <button onClick={() => navigate("/")}>Go Back</button>
+    </>
+  )
+}
+
+export default App
